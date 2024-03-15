c50cf2608985c154927a3b45d223cdb5ab66fba8
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-15 11:23:13 -0700

    add redirects  for netlify

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ChatMemory from '../views/ChatMemory.vue'
-import RAG from '../views/RAG.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ChatMemory from "../views/ChatMemory.vue";
+import RAG from "../views/RAG.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/chat-memory',
-      name: 'chat-memory',
-      component: ChatMemory
+      path: "/chat-memory",
+      name: "chat-memory",
+      component: ChatMemory,
     },
     {
-      path: '/rag',
-      name: 'rag',
-      component: RAG
-    }
-  ]
-})
+      path: "/rag",
+      name: "rag",
+      component: RAG,
+    },
+  ],
+});
 
-export default router
+export default router;
