f971b17f1f968fcaceeac05a4f67c0dbc0099862
razorcx-courses <100399176+razorcx-courses@users.noreply.github.com>
2024-03-15 14:21:42 -0700

    add vite config for tiktoken

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from "node:url";
-
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://github.com/mozilla/pdf.js/issues/17245
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), wasm(), topLevelAwait()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
